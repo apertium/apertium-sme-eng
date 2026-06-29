0e5addc2dafb81dbf29875f25d7b2d7e9ebf262c
Flammie A Pirinen <flammie@iki.fi>
2026-06-29 15:56:32 +0200

    fixes

--- dev/wmt.bash
+++ dev/wmt.bash
@@ -3,6 +3,6 @@ if ! test -d wmt-venv ; then
     python -m venv wmt-venv
 fi
 wmt-venv/bin/activate
-pip install 'mt[hf,xlsx]==0.5.1'
+pip install 'mtdata[hf,xlsx]==0.5.1'
 wget https://www.statmt.org/wmt26/mtdata/mtdata.recipes.wmt26-constrained.yml
 mtdata get-recipe -ri "wmt26-eng-sme" -o "wmt26-eng-sme" --compress --no-merge -j 12
